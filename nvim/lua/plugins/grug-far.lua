83e3661c7beaecac4a6b038fcd9a4721d334215d
marko-obradovic <marko@obradovic.co.uk>
2025-10-27 17:08:54 +0000

    Change GrugFar and Harpoon keybinds

--- nvim/lua/plugins/grug-far.lua
+++ nvim/lua/plugins/grug-far.lua
@@ -1,11 +1,10 @@
 -- grug-far.nvim is a Neovim plugin that provides an enhanced
 -- “find and replace” interface (like :s but smarter and easier to use).
 return {
-  'MagicDuck/grug-far.nvim',
-  config = function()
-    -- optional setup
-    -- you could override defaults like:
-    -- vim.g.grug_far = { ... }
-  end,
+	"MagicDuck/grug-far.nvim",
+	config = function()
+		vim.keymap.set("n", "<leader>gf", function()
+			require("grug-far").open()
+		end, { desc = "Open GrugFar (Find and Replace)" })
+	end,
 }
-
